e8c10af60e699f4ef0566f3aee3472cba62a857f
Timothy Delille <timothydelille@aol.com>
2024-12-07 19:29:58 -0800

    fix

--- components/rsvp.js
+++ components/rsvp.js
@@ -4,21 +4,22 @@ export default function Rsvp() {
     const [query, setQuery] = useState("");
     const [guests, setGuests] = useState([]);
     const [filteredGuests, setFilteredGuests] = useState([]);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [selectedGuest, setSelectedGuest] = useState(null);
-  
+
     // Load all guests on component mount
-    useEffect(() => {
+    const loadGuests = async () => {
       fetch('/api/getAllGuests')
-        .then(response => response.json())
-        .then(data => {
-          if (data.guests) {
-            setGuests(data.guests);
-          } else if (data.message) {
-            console.error('Error fetching guests:', data.message);
-          }
-        })
-        .catch(error => console.error('Error fetching guests:', error));
-    }, []);
+      .then(response => response.json())
+      .then(data => {
+        if (data.guests) {
+          setGuests(data.guests);
+        } else if (data.message) {
+          console.error('Error fetching guests:', data.message);
+        }
+      })
+      .catch(error => console.error('Error fetching guests:', error));
+    }
   
     // Filter guests on query change
     const search = (event) => {
@@ -60,22 +61,50 @@ export default function Rsvp() {
         }
       }
     };
-  
+    
+    const handleCloseModal = () => {
+      setSelectedGuest(null);
+      setQuery("");
+    }
+
+    const handleOnFocus = async () => {
+      await loadGuests();
+      setIsDropdownOpen(true);
+    }
+
+    const handleOnBlur = () => {
+      setIsDropdownOpen(false);
+    }
     return (
       <div className="w-10/12 mx-auto">
-        <p className={"text-2xl"} id="#RSVP">RSVP</p>
-        <div className="relative" onKeyPress={handleKeyPress}>
+        <p className="text-2xl mb-2" id="#RSVP">RSVP</p>
+        <div className="relative" onKeyPress={handleKeyPress} onBlur={handleOnBlur}>
+          <div className="px-2 py-1 flex flex-row gap-x-2 border border-neutral-300 focus:border-orange-100 rounded-md w-full">
           <input
-            className='bg-transparent border border-neutral-300 rounded-md px-2 py-1 w-full'
+            // outline-none prevents the blue ring when the input is in focus.
+            className='bg-transparent outline-none w-full'
             type="text"
             placeholder="e.g. Jensen Huang"
             onChange={search}
+            // why not use useEffect instead? if I rsvp in the modal, close it
+            // and search for my name again, i want to see the updated value.
+            onFocus={handleOnFocus}
             value={query}
           />
+
+          <button
+            onClick={() => {setQuery(""); setFilteredGuests([]);}}
+            className="text-gray-600"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+          </div>
           
           {/* guests dropdown */}
-          <Dropdown guests={filteredGuests} selectGuest={selectGuest} />
-          {selectedGuest && <RsvpModal selectedGuest={selectedGuest} closeFunction={() => setSelectedGuest(null)}/>}
+          { <Dropdown guests={filteredGuests} selectGuest={selectGuest} /> }
+          {selectedGuest && <RsvpModal selectedGuest={selectedGuest} closeFunction={handleCloseModal}/>}
         </div>
       </div>
     );
@@ -112,18 +141,6 @@ const RsvpModal = ({ selectedGuest, closeFunction }) => {
   if (!selectedGuest) { 
     return <></>
   }
-
-  // const fetchRsvp = async () => {
-  //   const response = await fetch(`/api/getSingleGuest?id=${selectedGuest.id}`);
-  //   const { guest, message } = await response.json();
-
-  //   if (guest) {
-  //     return guest.rsvp;
-  //   } else {
-  //     console.error('Error fetching guest:', message);
-  //   }
-  //   return null;
-  // }
 
   const setRsvp = async (id, rsvp) => {
     try {
@@ -172,18 +189,17 @@ const RsvpModal = ({ selectedGuest, closeFunction }) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="bg-white rounded-lg p-4 w-5/6 mt-5 mb-auto flex flex-col">
         <div className="flex flex-row justify-between">
-          <p className="text-lg font-semibold">{selectedGuest.name}</p>
+          <p className="text-lg font-light">{selectedGuest.name}</p>
           <button
             onClick={closeFunction}
             className="text-gray-600"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
-        <div className="text-center mt-5">
-          {/* <h2 className="text-lg font-semibold mb-2">RSVP Status</h2> */}
+        <div className="text-center mt-5 flex flex-col h-full justify-center items-center">
           {
             loading && <div className="flex justify-center"><LoadSpinner/></div>
           }
@@ -191,35 +207,22 @@ const RsvpModal = ({ selectedGuest, closeFunction }) => {
           {/* Missing RSPV */}
           { !loading && rsvp === null &&
           <>
-          <p className="text-xl">Aucune réponse 🤷</p>
-          <div className="flex flex-row justify-around">
-            <button
-              className="bg-green-500 text-white px-4 py-2 rounded-md mt-4"
-              onClick={() => handleRsvp(true)}
-            >
-              Je viens
-            </button>
-            <button
-              className="bg-red-500 text-white px-4 py-2 rounded-md mt-4"
-              onClick={() => handleRsvp(false)}
-            >
-              Je ne viens pas
-            </button>
-          </div>
+            <p className="text-lg font-light">Aucune réponse 🤷</p>
+            <div className="flex flex-row gap-x-3">
+              <ButtonYes handleRsvp={handleRsvp} />
+              <ButtonNo handleRsvp={handleRsvp} />
+            </div>
           </>
           }
-
           
           { !loading && rsvp &&
           <>
           <p className="text-xl">Présence confirmée! 🥳</p>
           <p className="mt-5 text-start text-gray-600">Changement de plan ?</p>
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
-            onClick={() => handleRsvp(false)}
-          >
-            Je ne viens plus
-          </button>
+          <div className="flex flex-row gap-x-3">
+            <ButtonNo handleRsvp={handleRsvp} />
+            <ButtonNull handleRsvp={handleRsvp} />
+          </div>
           </>
           }
 
@@ -227,12 +230,10 @@ const RsvpModal = ({ selectedGuest, closeFunction }) => {
             <>
             <p className="text-xl">Absence confirmée 😢</p>
             <p className="mt-5 text-start text-gray-600">Changement de plan ?</p>
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
-              onClick={() => handleRsvp(true)}
-            >
-              Je viens
-            </button>
+            <div className="flex flex-row gap-x-3">
+              <ButtonYes handleRsvp={handleRsvp} />
+              <ButtonNull handleRsvp={handleRsvp} />
+            </div>
             </>
           }
         </div>
@@ -241,9 +242,42 @@ const RsvpModal = ({ selectedGuest, closeFunction }) => {
   )
 }
 
+const ButtonYes = ({ handleRsvp }) => {
+  return (
+    <button
+      className="bg-green-100 hover:bg-green-200 border border-green-700 text-sm text-green-700 px-2 py-2 rounded-md mt-4"
+      onClick={() => handleRsvp(true)}
+    >
+      Je viens
+    </button>
+  )
+}
+
+const ButtonNo = ({ handleRsvp }) => {
+  return (
+    <button
+      className="bg-red-100 hover:bg-red-200 border border-red-700 text-sm text-red-700 px-2 py-2 rounded-md mt-4"
+      onClick={() => handleRsvp(false)}
+    >
+      Je ne viens pas
+    </button>
+  )
+}
+
+const ButtonNull = ({ handleRsvp }) => {
+  return (
+    <button
+      className="bg-neutral-100 hover:bg-neutral-200 border border-neutral-700 text-sm text-neutral-700 px-2 py-2 rounded-md mt-4"
+      onClick={() => handleRsvp(null)}
+    >
+      Je ne sais pas encore
+    </button>
+  )
+}
+
 const LoadSpinner = () => {
   return (
-    <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
       <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
     </svg>
